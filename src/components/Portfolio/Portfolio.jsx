3169b9ba334493d204ae2c12a6309f9532939b1d
Rum3 <rum3n1992@gmail.com>
2022-11-01 14:39:05 +0200

    relese

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,7 +2,9 @@ import React, { useContext } from "react";
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
+import snake from "../../img/snake.png"
 import TODO from "../../img/TODO.png";
+import math from "../../img/math.png"
 import calc from "../../img/calc.png";
 import budedge from "../../img/budedge.png";
 import calendar from "../../img/calendar.png";
@@ -24,13 +26,19 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={TODO} alt="" />
+          <a href="https://rum3.github.io/TODO_APP/"><img src={TODO} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={calc} alt="" />
+          <a href="https://rum3.github.io/mathForKids/"><img src={math} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={calendar} alt="" />
+          <a href="https://rum3.github.io/calculator/"><img src={calc} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="https://rum3.github.io/calendar/"><img src={calendar} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="https://rum3.github.io/snake/"><img src={snake} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
           <img src={budedge} alt="" />
